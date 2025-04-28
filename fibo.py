4ea5bef120554de8ccdb3d82c44b464eed40bc79
Hana Machalikova <hanamachalikova@seznam.cz>
2025-04-28 13:09:35 +0200

    Add fibo.py

--- fibo.py
+++ fibo.py
@@ -1,6 +1,12 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
-    pass
+    print(recursive_nth_fibo(2))
 
 
 if __name__ == "__main__":
